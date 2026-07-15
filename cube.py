706202e7e5da24b3ad4e57ee477f51c838d259d2
Dennis Colak <denniscolak08@gmail.com>
2026-07-15 19:08:31 -0400

    added slice moves

--- cube.py
+++ cube.py
@@ -2,6 +2,7 @@ import numpy as np
 
 # green forwards, white up
 F, U, L, D, R, B = range(6)
+M, E, S = range(6, 9) # middle slices
 COLORS = ["green", "white", "orange", "yellow", "red", "blue"]
 
 SOLVED = np.array([np.full((3, 3), side) for side in range(6)])
@@ -14,6 +15,12 @@ STRIPS = {
     B: [(U, 0, None, 1), (R, None, 2, 1), (D, 2, None, -1), (L, None, 0, -1)],
     R: [(U, None, 2, 1), (F, None, 2, 1), (D, None, 2, 1), (B, None, 0, -1)],
     L: [(U, None, 0, 1), (B, None, 2, -1), (D, None, 0, 1), (F, None, 0, 1)],
+}
+
+PAIRS = {
+    M: (R, L),
+    E: (U, D),
+    S: (B, F),
 }
 
 
@@ -37,6 +44,13 @@ INDICES = {
 
 
 def turn(cube, face, n=1):
+    # slices are two opposite faces at once, so centers never move
+    if face in PAIRS:
+        a, b = PAIRS[face]
+        turn(cube, a, n)
+        turn(cube, b, -n)
+        return # early
+
     n %= 4
     # face rotation
     cube[face] = np.rot90(cube[face], -n)
@@ -49,7 +63,7 @@ def turn(cube, face, n=1):
     # no need to return
 
 
-FACE_OF = {"F": F, "U": U, "L": L, "D": D, "R": R, "B": B}
+FACE_OF = {"F": F, "U": U, "L": L, "D": D, "R": R, "B": B, "M": M, "E": E, "S": S}
 SUFFIX = {"": 1, "'": -1, "2": 2}
 
 
--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 from cube import COLORS, SOLVED, convert_notation, record
 from visualize import animate
 
-ALG = "U B D' F2 D B' U' R2 D F2 D' R2 D F2 D' R2"
-INTERVAL = 100  # ms per move
+"""
+M E S are not standard notation. The centers dont move so
+M = R L'
+E = U D'
+S = F B'
+"""
+ALG = "R M' L'"
+INTERVAL = 400  # ms per move
 
 
 def main():
